f1482ed53eb9a7d479455a1f55173867367178dd
osaighi <84103168+osaighi@users.noreply.github.com>
2024-10-22 21:44:50 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1,8 +1,13 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 600;
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
 
 // Variables
 let playerWidth = 50;
